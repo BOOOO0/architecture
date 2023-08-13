7041f9d948148a88a70f1e96b46784a0ed389457
Buyoung Chung <sunarol@naver.com>
2023-08-13 16:22:28 +0900

    feat: POST 방식으로 값 입력

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GetInput from './components/GetInput.jsx'
+import PostInput from './components/PostInput.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
-
   return (
     <>
-      <GetInput/>
+      <PostInput/>
     </>
   )
 }
