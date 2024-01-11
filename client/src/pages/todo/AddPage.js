433f587012624427d3be0255e094d6e1bc366fa4
Buyoung Chung <sunarol@naver.com>
2024-01-11 23:11:03 +0900

    feat: TodoList 클라이언트 사이드
    일단 강의대로 리액트 라우터 위주로 훅 사용하면서
    라우팅, 파라미터 다루기 등 그대로 적용했고
    이제 서버 사이드 개발하면서 나중에 추가적으로 수정할듯
    만약에 수정사항 없이 이대로면 배포는 미뤄야할지도

---
+++ client/src/pages/todo/AddPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddPage(props) {
+  return (
+    <div className=" text-3xl font-extrabold">
+      <div>Add Page</div>
+    </div>
+  );
+}
+
+export default AddPage;
