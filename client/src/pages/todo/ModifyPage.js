a7cfb11f5050bce9cefc9b3ddc90c1802ed08728
Buyoung Chung <sunarol@naver.com>
2024-01-16 15:26:57 +0900

    feat: 삭제, 수정 컴포넌트 완성 Todolist 개발 완료

--- client/src/pages/todo/ModifyPage.js
+++ client/src/pages/todo/ModifyPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+import ModifyComponent from "../../components/todo/ModifyComponent";
 
 function ModifyPage(props) {
-  return <div>ModifyPage 페이지입니다.</div>;
+    const {tno} = useParams()
+
+    return (
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">
+                Todo Modify Page
+            </div>
+
+            <ModifyComponent tno={tno}/>
+
+        </div>
+    );
 }
 
 export default ModifyPage;
